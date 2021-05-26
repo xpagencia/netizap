c0a66dfcc78ae933dc28a124a73d35d46c17fdf8
Fabio Santos <fabiosantosrj@gmail.com>
2021-05-26 18:35:00 -0300

    incluido namespace de webhook

--- src/controllers/Netizap/netizap.webhook.ts
+++ src/controllers/Netizap/netizap.webhook.ts
@@ -1,13 +1,22 @@
-import { Config } from '@controllers/Netizap/netizap.config';
+import { Config, RequestZap } from '@controllers/Netizap/netizap.config';
 
 namespace Netizap{
   export class Webhook extends Config{
-    constructor(line: string) {
-      super(line);
-      console.log(`webhook`);
+    constructor(config: Config) {
+      super(config.line, config.port, config.app, config.accessKey);
+      this.entidade = "webhook";
     }
-    test() {
-      console.log("test");
+
+    authorize = async (enable: boolean, urlCallback: string, method: string = "post") => {
+      let url = this.url.replace('{metodo}', 'authorize');
+      let request = new RequestZap();
+      return await request.post(`${url}`, {enable, method, url: urlCallback});
+    }
+
+    list = async (protocol: string) => {
+      let url = this.url.replace('{metodo}', 'list');
+      let request = new RequestZap();
+      return await request.get(`${url}`);
     }
   }
 }
